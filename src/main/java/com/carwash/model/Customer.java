8b015857707187a0862324bc2b85351c8d88fb06
liyanda <litoncpt025@student.wethinkcode.co.za>
2026-08-17 22:49:02 +0200

    feat: implementing Customer specifics

--- src/main/java/com/carwash/model/Customer.java
+++ src/main/java/com/carwash/model/Customer.java
@@ -1,4 +1,24 @@
 package com.carwash.model;
 
+import java.time.LocalDate;
+
 public class Customer {
+    private final int id;
+    private final String name;
+    private String phone;
+    private String email;
+    private String memberStatus; // NONE, BASIC, PREMIUM
+    private LocalDate joinDate;
+
+    public Customer(int id, String name, String phone, String email, String memberStatus, LocalDate joinDate) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.memberStatus = memberStatus;
+        this.joinDate = joinDate;
+    }
+
+    
+
 }
