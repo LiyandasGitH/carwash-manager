7b172d4d5187df60fe7fd50080a1b93a1f09fc6d
liyanda <litoncpt025@student.wethinkcode.co.za>
2026-08-23 16:33:13 +0200

    feat: implementing Service and its tests

--- src/main/java/com/carwash/model/Service.java
+++ src/main/java/com/carwash/model/Service.java
@@ -1,4 +1,56 @@
 package com.carwash.model;
 
+import java.math.BigDecimal;
+
 public class Service {
+    private int id;
+    private String name;
+    private BigDecimal price;
+    private int durationMin;
+
+    public Service() {}
+
+    public Service(int id, String name, BigDecimal price, in durationMin) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.durationMin = durationMin;
+    }
+
+    private int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    private BigDecimal getPrice() {
+        return price;
+    }
+
+    private void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    private int getDurationMin() {
+        return durationMin;
+    }
+
+    private void setDurationMin(int duration) {
+        this.durationMin = duration;
+    }
+
+    @Override
+    public String toString() {
+        return name + " price is " + price;
+    }
 }
--- src/test/java/com/carwash/modeltests/ServiceTest.java
+++ src/test/java/com/carwash/modeltests/ServiceTest.java
@@ -1,1 +1,25 @@
 package com.carwash.modeltests;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jpiter.api.Assertions.*;
+
+import com.carwash.model.Service;
+
+public class ServiceTest {
+
+    @Test
+    void testGettersandSetters() {
+        Service service = new Service();
+
+        service.setId(23);
+        service.setName("surface");
+        //  remember to test for BigDecimal price
+        // service.setPrice();
+        service.setDurationMin(30);
+
+        assertEquals(23, service.getId());
+        assertEquals("surface", service.getName());
+        // assertEquals(50.00, service.getPrice());
+        assertEquals(30, service.getDurationMin());
+    }
+}
