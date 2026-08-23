4c3650bb5b14c9d61ae5d7babd6520a86ef2d771
liyanda <litoncpt025@student.wethinkcode.co.za>
2026-08-23 14:39:05 +0200

    feat: implementing Employee class and testing getters and setters of the class

--- src/main/java/com/carwash/model/Employee.java
+++ src/main/java/com/carwash/model/Employee.java
@@ -1,5 +1,45 @@
 package com.carwash.model;
 
 public class Employee {
+    private int id;
+    private String name;
+    private String role;
     
+    public Employee() {}
+
+    public Employee(int id, String name, String role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name; 
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override 
+    public String toString() {
+        return name + "is an employee, their role is " + role;
+    }
+
 }
--- src/test/java/com/carwash/modeltests/EmployeeTest.java
+++ src/test/java/com/carwash/modeltests/EmployeeTest.java
@@ -1,5 +1,29 @@
 package com.carwash.modeltests;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.carwash.model.Employee;
+
 public class EmployeeTest {
+
+    @Test
+    void testGettersandSetters() {
+        Employee employee = new Employee();
+
+        employee.setId(10);
+        employee.setName("Vayon Poole");
+        employee.setRole("Manager");
+
+        assertEquals(10, employee.getId());
+        assertEquals("Vayon Poole", employee.getName());
+        assertEquals("Manager", employee.getRole());
+    }
+    
+
+
+
+    
+
     
 }
