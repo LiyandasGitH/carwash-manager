9b5fe8b2645a28cb3cd3760643b50bfcfbcf9542
liyanda <litoncpt025@student.wethinkcode.co.za>
2026-08-23 11:18:34 +0200

    feat: implemented Vehicle class & Vehicle getters and setters test

--- src/main/java/com/carwash/model/Vehicle.java
+++ src/main/java/com/carwash/model/Vehicle.java
@@ -1,5 +1,74 @@
 package com.carwash.model;
 
 public class Vehicle {
-    
+    private int id;
+    private int customerId;
+    private String plate;
+    private String make;
+    private String model;
+    private String colour;
+
+    public Vehicle() {}
+
+    public Vehicle(int id, int customerId, String plate, String make, String model, String colour) {
+        this.id = id;
+        this.customerId = customerId;
+        this.plate = plate;
+        this.make = make;
+        this.model = model;
+        this.colour = colour;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    @Override
+    public String toString() {
+        return "number plate of customer " + customerId + " is " + plate;
+    }
 }
--- src/test/java/com/carwash/modeltests/VehicleTest.java
+++ src/test/java/com/carwash/modeltests/VehicleTest.java
@@ -1,5 +1,29 @@
 package com.carwash.modeltests;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.carwash.model.Vehicle;
+
 public class VehicleTest {
     
+    @Test
+    void testGettersandSetters() {
+        Vehicle vehicle = new Vehicle();
+
+        vehicle.setId(2);
+        vehicle.setCustomerId(202);
+        vehicle.setPlate("CA 987-654");
+        vehicle.setMake("Volkswagen");
+        vehicle.setModel("Polo");
+        vehicle.setColour("Blue");
+
+        assertEquals(2, vehicle.getId());
+        assertEquals(202, vehicle.getCustomerId());
+        assertEquals("CA 987-654", vehicle.getPlate());
+        assertEquals("Volkswagen", vehicle.getMake());
+        assertEquals("Polo", vehicle.getModel());
+        assertEquals("Blue", vehicle.getColour());
+        
+    }
 }
