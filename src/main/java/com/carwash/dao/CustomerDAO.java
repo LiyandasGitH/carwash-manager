c26e54a06644483eb13412e29cf69f66cf33ec33
liyanda <litoncpt025@student.wethinkcode.co.za>
2026-09-02 17:07:07 +0200

    feat: customerdao draft

--- src/main/java/com/carwash/dao/CustomerDAO.java
+++ src/main/java/com/carwash/dao/CustomerDAO.java
@@ -1,4 +1,28 @@
 package com.carwash.dao;
 
+import com.carwash.db.DBConnection;
+import com.carwash.model.Customer;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+//import static java.lang.StableValue.map;
+
+
 public class CustomerDAO {
+
+//    public List<Customer> findAll() throws SQLException {
+//        String sql = "SELECT * FROM customers ORDER BY name";
+//        List<Customer> result = new ArrayList<>();
+//        try (
+//                Connection conn = DBConnection.getConnection();
+//                PreparedStatement statement = conn.prepareStatement(sql);
+//                ResultSet resultSet = statement.executeQuery()) {
+//                while (resultSet.next()) {
+//                    result.add(map(resultSet));
+//                }
+//        }
+//        return result;
+//    }
 }
