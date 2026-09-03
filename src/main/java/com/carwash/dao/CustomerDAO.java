af382bd11fdfe5beaadcb786d91c78ae7fe6b139
liyanda <litoncpt025@student.wethinkcode.co.za>
2026-09-03 09:45:55 +0200

    feat: created map helper method

--- src/main/java/com/carwash/dao/CustomerDAO.java
+++ src/main/java/com/carwash/dao/CustomerDAO.java
@@ -7,22 +7,32 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-//import static java.lang.StableValue.map;
-
-
 public class CustomerDAO {
 
-//    public List<Customer> findAll() throws SQLException {
-//        String sql = "SELECT * FROM customers ORDER BY name";
-//        List<Customer> result = new ArrayList<>();
-//        try (
-//                Connection conn = DBConnection.getConnection();
-//                PreparedStatement statement = conn.prepareStatement(sql);
-//                ResultSet resultSet = statement.executeQuery()) {
-//                while (resultSet.next()) {
-//                    result.add(map(resultSet));
-//                }
-//        }
-//        return result;
-//    }
+    public List<Customer> findAll() throws SQLException {
+        String sql = "SELECT * FROM customers ORDER BY name";
+        List<Customer> result = new ArrayList<>();
+        try (
+                Connection conn = DBConnection.getConnection();
+                PreparedStatement statement = conn.prepareStatement(sql);
+                ResultSet resultSet = statement.executeQuery()) {
+                while (resultSet.next()) {
+                    result.add(map(resultSet));
+                }
+        }
+        return result;
+    }
+
+    private Customer map(ResultSet resultSet) throws SQLException {
+        Customer c = new Customer();
+        c.setId(resultSet.getInt("id"));
+        c.setName(resultSet.getString("name"));
+        c.setPhone(resultSet.getString("phone"));
+        c.setEmail(resultSet.getString("email"));
+        c.setMemberStatus(resultSet.getString("member_status"));
+
+        Date d = resultSet.getDate("join_date");
+        c.setJoinDate(d != null ? d.toLocalDate() : null);
+        return c;
+    }
 }
