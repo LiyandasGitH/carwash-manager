9af7bb0371eb3972d5b6cbc8a9d16df697f1d532
liyanda <tonisiliyanda@gmail.com>
2026-09-20 01:07:46 +0200

    fix: making CP inherit from Swing container JPanel to correct UI methods

--- src/main/java/com/carwash/ui/CustomerPanel.java
+++ src/main/java/com/carwash/ui/CustomerPanel.java
@@ -7,9 +7,7 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
-import static java.awt.AWTEventMulticaster.add;
-
-public class CustomerPanel {
+public class CustomerPanel extends JPanel {
 
     private final CustomerDAO customerDAO = new CustomerDAO();
     private final VehicleDAO vehicleDAO = new VehicleDAO();
