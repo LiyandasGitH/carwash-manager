76434c77596fd72acaa467568c7ab4290f0aaf65
liyanda <tonisiliyanda@gmail.com>
2026-09-21 13:27:57 +0200

    refactor: payments table update

--- src/main/resources/sql/payments.sql
+++ src/main/resources/sql/payments.sql
@@ -1,9 +1,9 @@
 CREATE TABLE payments (
-    id INTEGER NOT NULL PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT NOT NULL PRIMARY KEY,
     ticket_id INTEGER NOT NULL,
-    amount NUMERIC(12, 2) NOT NULL,
-    payment_method VARCHAR(50) NOT NULL,
-    payment_status VARCHAR(50) NOT NULL,
-    paid_at DATETIME NOT NULL,
+    amount DECIMAL(6, 2) NOT NULL,
+    payment_method VARCHAR(20) NOT NULL,
+    payment_status VARCHAR(20) NOT NULL,
+    paid_at DATETIME NOT NULL DEFAULT (CURRENT_TIMESTAMP),
     FOREIGN KEY (ticket_id) REFERENCES tickets(id)
 );
