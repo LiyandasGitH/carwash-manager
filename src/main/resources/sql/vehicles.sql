a2daaa37c2cf93271540285ec2eaa2b0d7f7b45b
liyanda <tonisiliyanda@gmail.com>
2026-09-23 13:57:04 +0200

    fix: fixed syntax error

--- src/main/resources/sql/vehicles.sql
+++ src/main/resources/sql/vehicles.sql
@@ -1,8 +1,8 @@
 CREATE TABLE vehicles (
     id INTEGER NOT NULL PRIMARY KEY,
     customer_id INTEGER NOT NULL,
-    plate VARCHAR NOT NULL,
-    make VARCHAR NOT NULL,
-    colour VARCHAR NOT NULL,
+    plate VARCHAR(20) NOT NULL,
+    make VARCHAR(50) NOT NULL,
+    colour VARCHAR(50) NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES customers(id)
 );
